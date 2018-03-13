7a1283b01a98097f90c47de18ba26d67489622ad
Diellza Cunaj <diellzacunaj@gmail.com>
2018-03-13 23:07:01 +0100

    Diellza Cunaj- Added Bottom Navigation View

--- app/src/main/java/diellza/touristguide/MainActivity.java
+++ app/src/main/java/diellza/touristguide/MainActivity.java
@@ -1,13 +1,48 @@
 package diellza.touristguide;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_scan:
+                    mTextMessage.setText(R.string.title_scan);
+                    return true;
+                case R.id.navigation_monuments:
+                    mTextMessage.setText(R.string.title_monuments);
+                    return true;
+                case R.id.navigation_favorites:
+                    mTextMessage.setText(R.string.title_favorite);
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.mainBN);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 }
