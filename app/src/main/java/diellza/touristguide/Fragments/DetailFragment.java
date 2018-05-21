57dc7a6b61bfc6bae99cadc4c6782bd83352afb0
Ali Morina <alimorina@hotmail.com>
2018-05-21 12:16:13 +0200

    Ali Morina - added Listeners for directions image button and directions text. It will get the current position and Latitude and Longitude from database for the selected position and open GoogleMaps to show the route to the desired location

--- app/src/main/java/diellza/touristguide/Fragments/DetailFragment.java
+++ app/src/main/java/diellza/touristguide/Fragments/DetailFragment.java
@@ -1,5 +1,8 @@
 package diellza.touristguide.Fragments;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.CollapsingToolbarLayout;
@@ -15,16 +18,18 @@ import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 
+import java.util.Locale;
+
 import diellza.touristguide.Models.Monument;
 import diellza.touristguide.R;
 
 
 public class DetailFragment extends Fragment {
-    public static final String TAG="diellza.touristguide.Fragments.DetailFragment";
+    public static final String TAG = "diellza.touristguide.Fragments.DetailFragment";
 
-   ImageView itemDetailImg;
+    ImageView itemDetailImg;
     ImageView directions;
-    TextView addressTxt, uniqueNumberTxt, nameTxt, periodTxt, centuryTxt, classTxt, typeTxt, historyTxt;
+    TextView addressTxt, uniqueNumberTxt, nameTxt, periodTxt, centuryTxt, classTxt, typeTxt, historyTxt, directionsTxt;
     Monument monument;
     CollapsingToolbarLayout layout;
 
@@ -62,7 +67,8 @@ public class DetailFragment extends Fragment {
         classTxt = v.findViewById(R.id.classTextView);
         typeTxt = v.findViewById(R.id.typeTextView);
         historyTxt = v.findViewById(R.id.historyTextView);
-        layout=v.findViewById(R.id.toolbar_layout);
+        layout = v.findViewById(R.id.toolbar_layout);
+        directionsTxt = v.findViewById(R.id.getDirectionsTxt);
 
         FloatingActionButton fab = (FloatingActionButton) v.findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -73,9 +79,9 @@ public class DetailFragment extends Fragment {
             }
         });
 
-      layout.setTitle(monument.getTitle());
-       // getActivity().setTitle(monument.getTitle());
-        uniqueNumberTxt.setText(monument.getID()+"");
+        layout.setTitle(monument.getTitle());
+        // getActivity().setTitle(monument.getTitle());
+        uniqueNumberTxt.setText(monument.getID() + "");
         nameTxt.setText(monument.getTitle());
         historyTxt.setText(monument.getHistory());
         addressTxt.setText(monument.getAddress());
@@ -83,24 +89,56 @@ public class DetailFragment extends Fragment {
         centuryTxt.setText(monument.getCentury());
         classTxt.setText(monument.getClassName());
         typeTxt.setText(monument.getType());
-       // itemDetailImg.setImageResource(monument.getHistoryImg());
-        Glide.with(getContext()).load(monument.getOverviewImg()).override(500,220).into(itemDetailImg);
+
+        directions.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String destLabel = monument.getTitle();
+                /**
+                 * By the standards of Google, we must first give the parameters of Latitude
+                 * then Longitude, but when the database was created we swapped the parameters
+                 * unknowingly, so the parameters given in the line below are swapped also
+                 * to give the correct location
+                 * It's the same rule for @directionsTxt listener
+                 * */
+                String uri = String.format(Locale.ENGLISH, "http://maps.google.com/maps?daddr=%f,%f (%s)", monument.getLongitude(), monument.getLatitude(), destLabel);
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+                mapIntent.setClassName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity");
+                //checks if you have installed Google Maps
+                if (mapIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+                    getContext().startActivity(mapIntent);
+                }
+            }
+        });
+
+        directionsTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String destLabel = monument.getTitle();
+                String uri = String.format(Locale.ENGLISH, "http://maps.google.com/maps?daddr=%f,%f (%s)", monument.getLongitude(), monument.getLatitude(), destLabel);
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+                mapIntent.setClassName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity");
+                if (mapIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+                    getContext().startActivity(mapIntent);
+                }
+            }
+        });
+
+        // itemDetailImg.setImageResource(monument.getHistoryImg());
+        Glide.with(getContext()).load(monument.getOverviewImg()).override(500, 220).into(itemDetailImg);
 
         return v;
     }
 
 
-
-    public static DetailFragment newInstance(Monument monument)
-    {
-        DetailFragment detailFragment=new DetailFragment();
-        Bundle bundle=new Bundle();
+    public static DetailFragment newInstance(Monument monument) {
+        DetailFragment detailFragment = new DetailFragment();
+        Bundle bundle = new Bundle();
         bundle.putSerializable(TAG, monument);
         detailFragment.setArguments(bundle);
         return detailFragment;
 
     }
-
 
 
 }
